b7712b88696e655cb85a04dab7c344709646b6af
vikash kumar <vikashkumar24041997@gmail.com>
2025-09-01 23:58:17 +0530

    query imp

--- query/index.js
+++ query/index.js
@@ -6,9 +6,46 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+const posts = {};
+/* EXAMPLE
 
-app.post("/events", (rea, res) => {});
+posts === {
+  'jiabj':{
+    id:'jiabj,
+    title:'post title',
+    comments: [
+    {id:'kaio' , comment : 'comment'}
+    ]
+  },
+}
+
+
+
+*/
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on 4002");

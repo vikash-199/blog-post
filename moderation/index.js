7fd42faa86e24b9f6924166d4dc065423e2fd769
vikash kumar <vikashkumar24041997@gmail.com>
2025-09-03 19:58:45 +0530

    moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,3 +5,25 @@ const axios = require("axios");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log("Listen at 4003");
+});
